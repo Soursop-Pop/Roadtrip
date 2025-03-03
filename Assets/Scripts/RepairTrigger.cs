a478107fea968a04c5073a6ff7bcbcdbaa803cb5
Alex Tang <alexandratang09@gmail.com>
2025-03-03 12:22:16 -0500

    added dialogue trigger in world with minimal terrain

---
+++ Assets/Scripts/RepairTrigger.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RepairTrigger : MonoBehaviour
+{
+    public VehicleController vehicle;
+    private bool playerNearby = false;
+
+    void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(KeyCode.F))
+        {
+            StartMinigame();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+            UIManager.ShowRepairPrompt(true); // Show UI "Press E to Fix"
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+            UIManager.ShowRepairPrompt(false); // Hide UI
+        }
+    }
+
+    void StartMinigame()
+    {
+        SceneManager.LoadScene("PipeRotationPuzzleTest", LoadSceneMode.Additive);
+    }
+
+    public void EndMinigame()
+    {
+        SceneManager.UnloadSceneAsync("PipeRotationPuzzleTest");
+        vehicle.Repair();
+    }
+}
